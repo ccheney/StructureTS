958d506eb3031c6ea3a5b5d832d5f590914ba016
codebelt <code@codebelt.com>
2014-03-16 22:08:17 -0500

    Get simon example working.

--- examples/Simon/src/assets/scripts/SimonApp.ts
+++ examples/Simon/src/assets/scripts/SimonApp.ts
@@ -33,6 +33,24 @@ module codeBelt
         constructor()
         {
             super();
+
+
+            /*var tl = new TimelineLite,
+                mySplitText = new SplitText("#quote", {type:"words,chars"}),
+                chars = mySplitText.chars; //an array of all the divs that wrap each character
+
+            TweenLite.set("#quote", {perspective:400});
+
+            tl.staggerFrom(chars, 0.8, {opacity:0, scale:0, y:80, rotationX:180, transformOrigin:"0% 50% -50",  ease:Back.easeOut}, 0.01, "+=0", allDone);
+
+            function allDone(){
+                mySplitText.revert();
+            }
+
+            document.getElementById("animate").onclick = function() {
+                tl.restart();
+            }
+*/
         }
 
         /**
